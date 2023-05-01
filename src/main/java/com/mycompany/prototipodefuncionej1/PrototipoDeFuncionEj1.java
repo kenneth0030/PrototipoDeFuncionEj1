7b93394a99843b3e3d1dd77e2d6b4a840038e7f1
kenneth pegueros <kenneth_183@hotmail.com>
2023-05-01 16:36:43 -0600

    Initial commit

---
+++ src/main/java/com/mycompany/prototipodefuncionej1/PrototipoDeFuncionEj1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.prototipodefuncionej1;
+
+/**
+ *
+ * @author kenne
+ */
+public class PrototipoDeFuncionEj1 {
+
+    static int resultado = sumar(5, 3);
+
+    public static int sumar(int a, int b) {
+
+        return a + b;
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("el resultado de la suma es =" + resultado);
+
+    }
+}
